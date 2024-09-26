3ef1cb598641f05be3b126524c3259c3f74c8975
rafang2206 <rafang22@hotmail.com>
2024-09-25 20:22:25 -0500

    Add: adding module nest config and env variables

--- api-nest/src/app.module.ts
+++ api-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { WalletsModule } from './wallets/wallets.module';
 import { BuysModule } from './buys/buys.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, WalletsModule, BuysModule],
+  imports: [ConfigModule.forRoot(), UsersModule, WalletsModule, BuysModule],
   controllers: [],
   providers: [],
 })
--- api-nest/src/main.ts
+++ api-nest/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import configuration from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(configuration().port);
 }
 bootstrap();
