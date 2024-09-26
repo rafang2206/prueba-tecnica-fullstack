9484f6b8ea3c0f6ac289eb3e04f1093625cd8abc
rafang2206 <rafang22@hotmail.com>
2024-09-26 13:24:53 -0500

    Adding Cors to API Rest

--- api-rest/src/config/configuration.ts
+++ api-rest/src/config/configuration.ts
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface IConfiguration {
+  FRONTEND_URL: string;
   SERVER_URL: string;
   PORT: number;
 }
 
 export const CONFIGURATION: IConfiguration = {
+  FRONTEND_URL: process.env.FRONTEND_URL || '',
   SERVER_URL: process.env.SERVER_URL || '',
   PORT: +process.env.PORT! || 3000,
 };
